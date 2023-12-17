9a467b8b9ef86df4048b7974f13ccd94d03b4054
tahapek5454 <tahapek5454@gmail.com>
2023-12-17 15:20:52 +0300

    feat: add css for navbar link

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -1,6 +1,6 @@
 <template>
     <nav class="navbar d-flex align-items-center justify-content-between px-5 navbar-expand-lg bg-body-tertiary">
-        <RouterLink to="/">
+        <RouterLink to="/" style="text-decoration: none;">
             <h3 class="logo">Self Chat</h3>
         </RouterLink>
         <div>
@@ -13,12 +13,12 @@
 
         <div>
             <ul class="navbar-nav mb-lg-0">
-                <RouterLink to="/login" active-class="active">
+                <RouterLink to="/login" active-class="active" style="text-decoration: none;">
                     <li class="nav-item">
                         <span class="nav-link">Giriş Yap</span>
                     </li>
                 </RouterLink>
-                <RouterLink to="/register" active-class="active">
+                <RouterLink to="/register" active-class="active" style="text-decoration: none;">
                     <li class="nav-item">
                         <span class="nav-link">Üye Ol</span>
                     </li>
